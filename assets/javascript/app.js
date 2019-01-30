285a17b066ba161466c987d2a4e95d3763424ea2
chaosZeroFive <chaos.five.zero@gmail.com>
2019-01-30 14:51:21 -0600

    js complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,31 @@
 var questionArray = [{
         qString: "Which is the most predominant element in the Sun?",
         qChoice: ["Helium", "Iron", "Sulphur", "Hydrogen"],
-        qAnswer: 3
+        qAnswer: 3,
     },
 
     {
         qString: "Which is the closest active galaxy to the Milky Way?",
         qChoice: ["Sagittarius A", "Cygnus 11", "Orionis A", "Centaurus A"],
-        qAnswer: 3
+        qAnswer: 3,
     },
 
     {
         qString: "How many (official) planets are in our solar system?",
         qChoice: ["Twenty-Seven", "Nine", "Eight", "Fourteen"],
-        qAnswer: 2
+        qAnswer: 2,
     },
 
     {
         qString: "What is the closest star to our planet?",
         qChoice: ["Sun", "Alpha Centuari", "Proxima Centuari", "Jupiter"],
-        qAnswer: 0
+        qAnswer: 0,
     },
 
     {
         qString: "What is the product of Hydrogen fusion?",
         qChoice: ["Oxygen", "Helium", "Carbon", "Gold"],
-        qAnswer: 1
+        qAnswer: 1,
     }
 ];
 
@@ -36,40 +36,45 @@ var curQstCh1 = "";
 var curQstCh2 = "";
 var curQstCh3 = "";
 var curQstAns = "";
+var curQstImg = "";
 var numCorrect = 0;
 var numIncorrect = 0;
 var numMissed = 0;
 var interval;
 var time = 10;
+var counter = time;
 
 
 var messages = {
 	correct: {
 		txt: "That's Right!",
-		class: "alert-success",
+		class: "alert alert-success",
 	},
 	incorrect: {
 		txt: "Nope...That's Not It",
-		class: "alert-dark"
+		class: "alert alert-dark"
 	},
 	unanswered: {
 	txt: "Time is Up!",
-	class: "alert-danger"
+	class: "alert alert-danger"
 }
 }
 
-var alertBtn = "<button type='button' class='close' data-dismiss='alert' aria-label='Close' onClick='trans1()'><span aria-hidden='true'>Ok</span></button>";
+var alertBtn = "<button type='button' class='close' data-dismiss='alert' aria-label='Close' onClick='closeAlert()'><span aria-hidden='true'>Ok</span></button>";
 
 $(document).ready(function () {
     $("#next").hide();
 	$(".card-body").hide();
 	$(".alert").hide();
 	$(".scores").hide();
+    //console.log("@ document.ready | current index: " + curQstInd);
+    //console.log("@ document.ready | time: " + time);
+    //console.log("@ document.ready | counter: " + counter)
 });
 
 $("#start").on("click", function () {
     $("#start").hide();
-    console.log("@ start click | start clicked");
+    //console.log("@ start click | start clicked");
 	$(".card-body").show();
 	current();
 	
@@ -77,89 +82,119 @@ $("#start").on("click", function () {
 
 $(".custom-radio").on("click", ".custom-control-input", function(){
 	clearInterval(interval);
-	console.log($(this).val());
+	//console.log($(this).val());
 	var a = parseInt($(this).val());
 	if (a === curQstAns){
 		numCorrect++;
 		trans.transC();
-		//curQstInd++;
-		console.log("current index: " + curQstInd);
-		console.log("number correct: " + numCorrect);
+		//console.log("number correct: " + numCorrect);
 	}
 	else {
 		numIncorrect++;
-		console.log("number incorrect: " + numIncorrect);
+		//console.log("number incorrect: " + numIncorrect);
 		trans.transI();
-		//curQstInd++;
-		console.log("current index: " + curQstInd);
+		//console.log("current index: " + curQstInd);
 	}
 });
 
 var trans = {
 	
 	transC: function(){
-		console.log("trans.transC() called");
-		$(".alert").show().text(messages.correct.txt).addClass(messages.correct.class);
+		//console.log("trans.transC() called");
+		$(".alert").show().text(messages.correct.txt);
+        $(".alert").attr("class", messages.correct.class);
+        //$(".alert").toggleClass(messages.correct.class);
 		$(".alert").append(alertBtn);
 		$(".card").hide();
-		$('.scores').show();
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
-		curQstInd++;
-	
+        curQstInd++;
 	},
 	
 	transI: function(){
-		console.log("trans.transI() called");
-		$(".alert").show().text(messages.incorrect.txt).addClass(messages.incorrect.class);
+		//console.log("trans.transI() called");
+		$(".alert").show().text(messages.incorrect.txt);
+        $(".alert").attr("class", messages.incorrect.class);
+        //$(".alert").toggleClass(messages.incorrect.class);
 		$(".alert").append(alertBtn);
 		$(".card").hide();
-		$('.scores').show();
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
-		curQstInd++;
+        curQstInd++;
+        //console.log("@ transI | time: " + time);
+        //console.log("@ transI | counter: " + counter)
 	},
 	
 	transU: function(){
-		console.log("trans.transU() called");
-		$(".alert").show().text(messages.unanswered.txt).addClass(messages.unanswered.class);
+		//console.log("trans.transU() called");
+		$(".alert").show().text(messages.unanswered.txt);
+        $(".alert").attr("class", messages.unanswered.class);
+        //$(".alert").toggleClass(messages.unanswered.class);
 		$(".alert").append(alertBtn);
 		$(".card").hide();
-		$('.scores').show();
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
-		curQstInd++;
+        curQstInd++;
+        //console.log("@ transU | time: " + time);
+        //console.log("@ transU | counter: " + counter)
 	}
 	
 }
 
-function trans1(){
-	console.log("trans1() called");
-	$("input[type='radio']").prop("checked", false);
-	$(".card").show();
-	$('.scores').hide();
-	current();
-	
-	
+function closeAlert(){
+    if (curQstInd < questionArray.length){
+        //console.log("question " + curQstInd + " of " + questionArray.length);
+        counter = time;
+        $(".card-header").html(counter);
+        $(".alert").empty();
+        //console.log("closeAlert() called");
+        $("input[type='radio']").prop("checked", false);
+        $(".card").show();
+        //$('.scores').hide();
+        current();
+        //console.log("@ closeAlert | time: " + time);
+        //console.log("@ closeAlert | counter: " + counter)
+        //console.log("*************************")
+        $(".alert").attr("class", "alert");
+    }
+    else {
+        gameOver();
+    }
+}
+
+function gameOver(){
+    $(".card").show();
+    $(".card-header").html("<h3>Lets See How You Did</h3>");
+    $(".card-title").hide();
+    $(".card-body").hide();
+    $(".card-footer").append("<h4>" + numCorrect + " Correct Answers</h4>");
+    $(".card-footer").append("<h4>" + numIncorrect + " Incorrect Answers</h4>");
+    $(".card-footer").append("<h4>" + numMissed + " Unanswered Questions</h4>");
+    $('.scores').hide();
+    $("#start").show();
 }
 
 function current(){
-	console.log("current() called");
-	curQstStr = questionArray[curQstInd].qString;
-	curQstCh0 = questionArray[curQstInd].qChoice[0];
-	curQstCh1 = questionArray[curQstInd].qChoice[1];
-	curQstCh2 = questionArray[curQstInd].qChoice[2];
-	curQstCh3 = questionArray[curQstInd].qChoice[3];
-	curQstAns = questionArray[curQstInd].qAnswer;
+	//console.log("current() called");
+    var i = questionArray[curQstInd];
+	curQstStr = i.qString;
+	curQstCh0 = i.qChoice[0];
+	curQstCh1 = i.qChoice[1];
+	curQstCh2 = i.qChoice[2];
+	curQstCh3 = i.qChoice[3];
+	curQstAns = i.qAnswer;
 	interval = setInterval(timer, 1000);
 	setCurrent();
+    //console.log("@ current() | current index: " + curQstInd);
+    //console.log("@ current | time: " + time);
+   //console.log("@ current | counter: " + counter)
 }
 
 function setCurrent(){
-	console.log("setCurrent() called");
+	//console.log("setCurrent() called");
 	$(".scores").hide();
 	$(".card").show();
 	$(".card-title").html(curQstStr);
@@ -171,15 +206,17 @@ function setCurrent(){
 	$("#choice1").attr("value", 1);
 	$("#choice2").attr("value", 2);
 	$("#choice3").attr("value", 3);
-	$(".card-header").html(time);
+	$(".card-header").html(counter);
 	timer(interval);
+    //console.log("@ setCurrent | time: " + time);
+    //console.log("@ setCurrent | counter: " + counter)
 }
 
 function timer(){
-	if (time >= 1){
-		console.log("time remaining: " + time);
-		time--;
-		$(".card-header").html(time);
+	if (counter >= 1){
+		console.log("time remaining: " + counter);
+		counter--;
+		$(".card-header").html(counter);
 	}
 	else {
 		clearInterval(interval);
@@ -187,4 +224,3 @@ function timer(){
 		trans.transU();
 	}
 }
-
