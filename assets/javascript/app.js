326dd4661e8a79b775726bfce2a8be7b89b8e40f
Alton Carroll <chaos.five.zero@gmail.com>
2019-02-18 13:48:35 -0600

    revised features

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,7 +44,6 @@ var interval;
 var time = 10;
 var counter = time;
 
-
 var messages = {
 	correct: {
 		txt: "That's Right!",
@@ -57,55 +56,52 @@ var messages = {
 	unanswered: {
 	txt: "Time is Up!",
 	class: "alert alert-danger"
-}
+	}
 }
 
 var alertBtn = "<button type='button' class='close' data-dismiss='alert' aria-label='Close' onClick='closeAlert()'><span aria-hidden='true'>Ok</span></button>";
 
 $(document).ready(function () {
-    $("#next").hide();
-	$(".card-body").hide();
-	$(".alert").hide();
-	$(".scores").hide();
-    //console.log("@ document.ready | current index: " + curQstInd);
-    //console.log("@ document.ready | time: " + time);
-    //console.log("@ document.ready | counter: " + counter)
+    $('.btn-primary').html('Start');
+	$("#trivia").hide();
+	$("#alert-row").hide();
+	$("#scores").hide();
+    console.log("@ document.ready | current index: " + curQstInd);
+    console.log("@ document.ready | time: " + time);
+    console.log("@ document.ready | counter: " + counter)
 });
 
-$("#start").on("click", function () {
-    $("#start").hide();
-    //console.log("@ start click | start clicked");
-	$(".card-body").show();
+$(".btn-primary").on("click", function () {
+    $("#btn").hide();
+	$("#trivia").show();
 	current();
 	
 });
 
 $(".custom-radio").on("click", ".custom-control-input", function(){
 	clearInterval(interval);
-	//console.log($(this).val());
 	var a = parseInt($(this).val());
 	if (a === curQstAns){
 		numCorrect++;
 		trans.transC();
-		//console.log("number correct: " + numCorrect);
 	}
 	else {
 		numIncorrect++;
-		//console.log("number incorrect: " + numIncorrect);
 		trans.transI();
-		//console.log("current index: " + curQstInd);
 	}
 });
 
 var trans = {
 	
 	transC: function(){
-		//console.log("trans.transC() called");
-		$(".alert").show().text(messages.correct.txt);
+		$("#trivia").hide();
+		$("#alert-row").show();
+		$("#scores").show();
+		$(".alert").text(messages.correct.txt);
         $(".alert").attr("class", messages.correct.class);
         //$(".alert").toggleClass(messages.correct.class);
 		$(".alert").append(alertBtn);
-		$(".card").hide();
+		/*$(".container").hide();*/
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
@@ -113,52 +109,47 @@ var trans = {
 	},
 	
 	transI: function(){
-		//console.log("trans.transI() called");
-		$(".alert").show().text(messages.incorrect.txt);
+		$("#trivia").hide();
+		$("#alert-row").show();
+		$("#scores").show();
+		$(".alert").text(messages.incorrect.txt);
         $(".alert").attr("class", messages.incorrect.class);
         //$(".alert").toggleClass(messages.incorrect.class);
 		$(".alert").append(alertBtn);
-		$(".card").hide();
+		/*$(".container").hide();*/
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
         curQstInd++;
-        //console.log("@ transI | time: " + time);
-        //console.log("@ transI | counter: " + counter)
 	},
 	
 	transU: function(){
-		//console.log("trans.transU() called");
-		$(".alert").show().text(messages.unanswered.txt);
+		$("#trivia").hide();
+		$("#alert-row").show();
+		$("#scores").show();
+		$(".alert").text(messages.unanswered.txt);
         $(".alert").attr("class", messages.unanswered.class);
         //$(".alert").toggleClass(messages.unanswered.class);
 		$(".alert").append(alertBtn);
-		$(".card").hide();
+		/*$(".card").hide();*/
 		$("#correct").html("Correct: " + numCorrect);
 		$("#incorrect").html("Incorrect: " + numIncorrect);
 		$("#missed").html("Missed: " + numMissed);
         curQstInd++;
-        //console.log("@ transU | time: " + time);
-        //console.log("@ transU | counter: " + counter)
 	}
 	
 }
 
 function closeAlert(){
     if (curQstInd < questionArray.length){
-        //console.log("question " + curQstInd + " of " + questionArray.length);
         counter = time;
-        $(".card-header").html(counter);
+        $(".timer").html(counter);
         $(".alert").empty();
-        //console.log("closeAlert() called");
         $("input[type='radio']").prop("checked", false);
-        $(".card").show();
-        //$('.scores').hide();
-        current();
-        //console.log("@ closeAlert | time: " + time);
-        //console.log("@ closeAlert | counter: " + counter)
-        //console.log("*************************")
+        $(".trivia").show();
+        $('#scores').hide();
         $(".alert").attr("class", "alert");
+		current();
     }
     else {
         gameOver();
@@ -166,19 +157,14 @@ function closeAlert(){
 }
 
 function gameOver(){
-    $(".card").show();
-    $(".card-header").html("<h3>Lets See How You Did</h3>");
-    $(".card-title").hide();
-    $(".card-body").hide();
-    $(".card-footer").append("<h4>" + numCorrect + " Correct Answers</h4>");
-    $(".card-footer").append("<h4>" + numIncorrect + " Incorrect Answers</h4>");
-    $(".card-footer").append("<h4>" + numMissed + " Unanswered Questions</h4>");
-    $('.scores').hide();
-    $("#start").show();
+    $("#timer").html("<h3>Lets See How You Did</h3>");
+    $("#trivia").hide();
+    $("#scores").html("<h3>Lets See How You Did!</h3><h4>" + numCorrect + " Correct Answers</h4><h4>" + numIncorrect + " Incorrect Answers</h4><h4>" + numMissed + " Unanswered Questions</h4>");
+	/*$('.btn-primary').html('Play Again');
+    $("#btn").show();*/
 }
 
 function current(){
-	//console.log("current() called");
     var i = questionArray[curQstInd];
 	curQstStr = i.qString;
 	curQstCh0 = i.qChoice[0];
@@ -188,16 +174,12 @@ function current(){
 	curQstAns = i.qAnswer;
 	interval = setInterval(timer, 1000);
 	setCurrent();
-    //console.log("@ current() | current index: " + curQstInd);
-    //console.log("@ current | time: " + time);
-   //console.log("@ current | counter: " + counter)
 }
 
 function setCurrent(){
-	//console.log("setCurrent() called");
 	$(".scores").hide();
-	$(".card").show();
-	$(".card-title").html(curQstStr);
+	$("#trivia").show();
+	$("#question").html(curQstStr);
 	$("label[for=choice0]").html(curQstCh0);
 	$("label[for=choice1]").html(curQstCh1);
 	$("label[for=choice2]").html(curQstCh2);
@@ -206,17 +188,15 @@ function setCurrent(){
 	$("#choice1").attr("value", 1);
 	$("#choice2").attr("value", 2);
 	$("#choice3").attr("value", 3);
-	$(".card-header").html(counter);
+	$(".header").html(counter);
 	timer(interval);
-    //console.log("@ setCurrent | time: " + time);
-    //console.log("@ setCurrent | counter: " + counter)
 }
 
 function timer(){
 	if (counter >= 1){
 		console.log("time remaining: " + counter);
 		counter--;
-		$(".card-header").html(counter);
+		$("#timer").html(counter);
 	}
 	else {
 		clearInterval(interval);
